1b9106fb5db424522bedaaa44ed6306298b82d52
Admin <ferhat.nouis@gmail.com>
2021-11-18 14:21:38 +0000

    update message

--- app.py
+++ app.py
@@ -9,7 +9,9 @@ app = Flask(__name__)
 @app.route("/")
 def movie():
     with sqlite3.connect("db/movie.db") as c:
-        return render_template('index.html', suggests=c.execute("""
+        return render_template('index.html', 
+           api_url="http://127.0.0.1:8000", 
+           suggests=c.execute("""
             SELECT ItemID, MovieTitle, ROUND(Rating, 1) FROM movies 
             ORDER BY Rating DESC
             LIMIT 10 
